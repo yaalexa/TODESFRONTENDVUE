b5f9e26b90a757fbb64c0956bc0e72c77f2ae8ce
yuliepalechor <sanyulie08@gmail.com>
2022-10-13 19:42:14 -0500

    crear rama

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,7 +8,7 @@
           <div class="row g-0">
             <div class="col-md-3 col-lg-5 d-none d-md-block">
               <img src="../assets/lgtb.png"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; " />
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
               <div class="card-body p-4 p-lg-5 text-black">
