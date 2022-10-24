24b0f665015fa16261a06844e9278beb9704bd4a
haroldeduardo <hehurtado9@misena.edu.co>
2022-10-24 09:19:07 -0500

    nueva publicacion funcionando falta la relaciones de  categoria-detalle categoria

--- src/views/Publicacionevento/Publicacionevento.vue
+++ src/views/Publicacionevento/Publicacionevento.vue
@@ -1,7 +1,8 @@
 <template>
 
     <div>
-        <h1>Mostar Publicaciones eventos</h1>
+        <h1 class="text-center">Mostar Publicaciones eventos</h1>
+        <hr>
        
         <b-table sticky-header striped hover class="text-black bg-white" :fields="encabezado" :items="categoria">
 
@@ -17,7 +18,7 @@
 
 
             <template v-slot:cell(eliminar)="data">
-                <b-button variant="danger" size="sm" @click="eliminar(data.id)">Eliminar</b-button>
+                <b-button variant="danger" size="sm" @click="EliminarPublicacion(data.id)">Eliminar</b-button>
             </template>
 
            
@@ -65,7 +66,7 @@ export default {
     },
     mounted() {
         this.getcategorias()
-
+        this.EliminarPublicacion()
 
 
 
@@ -81,9 +82,22 @@ export default {
 
         NuevaCategoria() {
             this.$router.push('Nuevapublicacionevento')
+        },
+
+        EliminarPublicacion(id){
+      
+            this.axios.delete("http://127.0.0.1:8000/api/publicacion/id").then((response) => {
+                this.publicacion = response.data;
+                console.log("www")
+               
+            })
+      
         }
 
+
     }
+
+    
 }
 
 </script>
