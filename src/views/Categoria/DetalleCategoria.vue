c3737022e0cba985ed526ba248b107999b5fbfc5
unknown <dfdorado41@misena.edu.co>
2022-10-27 20:17:24 -0500

    actualizacion rama harold

---
+++ src/views/Categoria/DetalleCategoria.vue
@@ -0,0 +1,72 @@
+<template>
+
+    <div>
+        <h1>Categoria</h1>
+    
+        
+        <b-button  variant="warning" @click="NuevaCategoria()">Nueva Categoria</b-button>
+          <b-table sticky-header striped hover class="text-black bg-white" :fields="encabezado" :items="categoria">
+    
+               
+                <template v-slot:cell(editar)="data">
+    
+                    <b-button variant="primary" size="sm" @click="editar(data.id)">Editar</b-button>
+                  
+                </template>
+    
+    
+                <template v-slot:cell(guardar)="data">
+                    <b-button size="sm" @click="guardar(data.id)">Guardar</b-button>
+                </template>
+
+    
+            </b-table>
+      
+    </div>
+    </template>
+    
+    <script>
+    
+    import axios from "axios"
+    //import { response } from "express";
+    // el axios permite  llamar  todas las  apis  que se hayan creado
+        export default {
+            name:"Mostrarcategorias",
+            data(){
+              return {
+                categoria:[],
+                encabezado:[
+            {key:"id",label:"Id"},
+            {key:"nombre",label:"Categoria"},
+            { key:"descripcion",label:"Descripcion"},
+            { key: "editar", label: "Editar" },
+            { key: "guardar", label: "Guardar" },
+              ]}},
+            components:{
+    
+            },
+            mounted(){
+                this.getcategorias()
+    
+                
+                
+    
+            },
+    
+            methods:{
+    
+                getcategorias(){
+                    this.axios.get("http://127.0.0.1:8000/api/categoria").then((response)=>
+                    {
+                       this.categoria=response.data;
+                    })
+                },
+    
+                NuevaCategoria(){
+                    this.$router.push('NuevaCategoria')
+                }
+                
+               
+            }
+        }
+    </script>
