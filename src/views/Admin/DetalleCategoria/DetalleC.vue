99ff25c248d7f02615ea1f52f2662b8ff3eaf0ea
felipe3-A <lenovo.esucre@gmail.com>
2022-11-12 11:31:47 -0500

    cambio en sidebar

---
+++ src/views/Admin/DetalleCategoria/DetalleC.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>Categorias</h1>
+</template>
+
+<script>
+
+import axios from "axios"
+import DetalleC from "./DetalleC.vue"
+
+export default {
+    name: "Mostrarcategorias",
+    data() {
+        return {
+            cate:{
+                nombre:"",   // aqui se inicializa lo que  hace la conexion   de html con js
+                descripcion:"",
+                
+            },
+            categoria: [],
+            encabezado: [
+                { key: "id", label: "Id Publicacion" },
+                { key: "nombre", label: "Id Categoria" },
+                
+
+                { key: "editar", label: "EDITAR" },
+                { key: "eliminar", label: "ELIMINAR" },
+                { key: "asignar", label: "CREAR" }
+
+
+            ],
+            name: '',
+            cat:'',
+            nameState: null,
+            submittedNames: [],
+
+        }
+
+    }
+}
+
+</script>
