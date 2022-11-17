2b982dc865894ea2888d47ec0446cfd8fae7a492
felipe3-A <lenovo.esucre@gmail.com>
2022-11-16 19:23:16 -0500

    archivo del sena

--- src/views/Archivo/creararchivo.vue
+++ src/views/Archivo/creararchivo.vue
@@ -1,51 +1,37 @@
 <template>
   <div>
 
-    <div class="col-md-11">
-      <form
-                    action="Nuevapublicacionevento"
-                    method="POST"
-                    enctype="multipart/form-data"
-                  >
-                    <input
-                      id="archivo"
-                      name="archivo"
-                      type="file"
-                      accept="application/img"
-                      @change="imagenobtener"
-                      required
-                      placeholder="url"
-                      class="form-control"
-                    />
-                    <br>
-                    <input name="subir" type="submit" value="Subir" />
-                  </form>
-    </div>
-
-    <br><br>
-    <Input label="Text input" type="text" placeholder="url" /><br><br>
-
-    <b-form-select v-model="selected" :options="options"></b-form-select>
- 
-    <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+    <input type="file" name="image" @change="GetImage">
+    <img :src="avatar" alt="Imagenes">
+    <a href="#" class="btn btn-success" @click.prevent="upload">Upload</a>
   </div>
-
 
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        selected: null,
-        options: [
-          { value: null, text: 'Please select an option' },
-          { value: 'a', text: 'This is First option' },
-          { value: 'b', text: 'Selected Option' },
-          { value: { C: '3PO' }, text: 'This is an option with object value' },
-          { value: 'd', text: 'This one is disabled', disabled: true }
-        ]
+
+    data(){
+      return{
+
+        avatar:null
       }
+    },
+
+    methods:{
+      GetImage(e){
+
+       let image= e.target.files[0];
+       let reader  = new FileReader();
+       reader.readAsDataURL(image);
+       reader.onload = e =>{
+      
+        this.avatar = e.target.result
+       }
+      },
+
+      axios.post('/upload',{'image':this.avatar})
+
     }
   }
 </script>
