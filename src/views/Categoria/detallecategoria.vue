2c8130e13699d266a5884037c135d8896cf016a8
felipe3-A <lenovo.esucre@gmail.com>
2022-11-22 19:53:25 -0500

    asiganacion11

--- src/views/Categoria/detallecategoria.vue
+++ src/views/Categoria/detallecategoria.vue
@@ -6,7 +6,30 @@
         <b-button variant="warning" @click="Publicacionevento()">Publicaciones</b-button>
         <b-button variant="primary" @click="Categoria()">Categorias</b-button>
 
-        
+        <div class="mt-3">
+            <div v-if="submittedNames.length === 0"></div>
+            <ul v-else class="mb-0 pl-3">
+                <li v-for="name in submittedNames">{{ name }}</li>
+            </ul>
+        </div>
+        <!--<b-modal id="modal-prevent-closing" ref="modal" title="Categoria" @show="resetModal" @hidden="resetModal"
+            @GuardarCategoria="handleOk">
+            <form ref="form" @submit.stop.prevent="handleSubmit">
+                <b-form-group label="Categoria" label-for="name-input" invalid-feedback="Name is required"
+                    :state="nameState">
+
+                    <b-form-input id="name-input" v-model="cate.nombre" :state="nameState" required></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Descripcion Categoria" label-for="Descripcion-input"
+                    invalid-feedback="Name is required" :state="nameState">
+
+                    <b-form-input id="Descripcion-input" v-model="cate.descripcion" :state="nameState" required></b-form-input>
+                </b-form-group>
+                <b-button c variant="primary" @click="GuardarCategoria()">REGISTRAR</b-button>
+            </form>
+        </b-modal> -->
+
         <b-table :fields="encabezado" :items="categoria">
 
             <template v-slot:cell(editar)="data">
@@ -77,7 +100,39 @@ export default {
         },
         Categoria() {
             this.$router.push('Categoria')
-        }},}
+        },
+        
+        checkFormValidity() {
+            const valid = this.$refs.form.checkValidity()
+            this.nameState = valid
+            return valid
+        },
+        resetModal() {
+            this.name = ''
+            this.nameState = null
+        },
+        handleOk(bvModalEvent) {
+            // Prevent modal from closing
+            bvModalEvent.preventDefault()
+            // Trigger submit handler
+            this.handleSubmit()
+        },
+        handleSubmit() {
+            // Exit when the form isn't valid
+            if (!this.checkFormValidity()) {
+                return
+            }
+            // Push the name to submitted names
+            this.submittedNames.push(this.name)
+            // Hide the modal manually
+            this.$nextTick(() => {
+                this.$bvModal.hide('modal-prevent-closing')
+            })
+        }
+
+
+    }
+}
 
 </script>
     
